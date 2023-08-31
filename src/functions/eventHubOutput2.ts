72771c6cac72ab7a9e6b8ce9e7fdd0cde365c082
Eric Jizba <erijiz@microsoft.com>
2023-08-30 19:23:56 -0700

    more samples

---
+++ src/functions/eventHubOutput2.ts
@@ -0,0 +1,16 @@
+import { app, InvocationContext, output, Timer } from '@azure/functions';
+
+export async function timerTrigger1(myTimer: Timer, context: InvocationContext): Promise<string[]> {
+    const timeStamp = new Date().toISOString();
+    const message = `Message created at: ${timeStamp}`;
+    return [`1: ${message}`, `2: ${message}`];
+}
+
+app.timer('timerTrigger1', {
+    schedule: '0 */5 * * * *',
+    return: output.eventHub({
+        eventHubName: 'myeventhub',
+        connection: 'MyEventHubSendAppSetting',
+    }),
+    handler: timerTrigger1,
+});
